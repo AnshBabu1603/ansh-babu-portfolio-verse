629b256d117634653233e07c8d1a1fced0b54d84
AnshBabu1603 <raj.anshbabu.ab@gmail.com>
2026-03-25 02:39:20 +0530

    Implement fixed background component

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,6 +1,15 @@
-
 const Background = () => {
-  return null;
+  return (
+    <div style={{ 
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      backgroundColor: "#0f172a",
+      zIndex: -1
+    }} />
+  );
 };
 
 export default Background;
